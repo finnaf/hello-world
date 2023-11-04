b318e85a66166dc8c12168a8a8b06347af32eb3a
Finn <110039821+finnaf@users.noreply.github.com>
2023-11-04 15:11:15 +0000

    basic java

---
+++ learn_java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    // comment
+    public static void main(String[] args){
+        // final == const
+        final String say = "Hello World";
+        float num = 5.94f;
+        System.out.println(say);
+        System.out.print(num * num);
+    }
+}
